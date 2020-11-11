4cb28891780d5a4ace1e3bafb291d2e194cd194b
yoonkyoungchoi <s2019w40@e-mirim.hs.kr>
2020-11-11 23:34:28 +0900

    first commit

---
+++ syntac/string.js
@@ -0,0 +1,7 @@
+var str = "gjsadgfjgujhvbnsgsngjfdkghisrdugnjkfdhjrejdkfsl"
+var a = str.length
+var name = "hello"
+console.log(a);
+console.log(str.length);
+console.log('음하하하핳하ㅏ핳'+'24');
+console.log(`My name is ${name} 너를 사랑해는 중잉다`)
